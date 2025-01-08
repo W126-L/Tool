7adfd9164a7040ca225cde282f7f7528e1d28c9f
zyzdai <73833115+zyzdai@users.noreply.github.com>
2025-01-08 21:14:37 +0800

    更新 91.js

--- Scripts/91.js
+++ Scripts/91.js
@@ -1,5 +1,5 @@
 var attach = {
-    "openUrl":"https://play.cdnmicrosoft.com/short_m3u8/MFewTFNAXlw=.m3u8",
+    "openUrl":"https://video.twimg.com/ext_tw_video/1876816664043667456/pu/pl/JBGogn26Zdi863kl.m3u8?tag=12",
     "mediaUrl":"https://img.dnnmr.cn/short_image/7ec057d11d0c4f3cb389db976ea0b5e4.jpg"
 }
 $notification.post("title","subtitle","content",attach)
