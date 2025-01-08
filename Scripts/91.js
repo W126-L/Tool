2420e5cbebb7ee1adb095d2f8c878dffbaad91e0
zyzdai <73833115+zyzdai@users.noreply.github.com>
2025-01-08 20:43:23 +0800

    创建 91.js

---
+++ Scripts/91.js
@@ -0,0 +1,5 @@
+var attach = {
+    "openUrl":"https://play.cdnmicrosoft.com/short_m3u8/MFewTFNAXlw=.m3u8",
+    "mediaUrl":"https://img.dnnmr.cn/short_image/7ec057d11d0c4f3cb389db976ea0b5e4.jpg"
+}
+$notification.post("title","subtitle","content",attach)
