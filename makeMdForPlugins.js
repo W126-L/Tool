37ff74e82522dbb8962022647647dd6ea8f91024
zyzdai <73833115+zyzdai@users.noreply.github.com>
2025-02-04 23:45:19 +0800

    更新 makeMdForPlugins.js

--- makeMdForPlugins.js
+++ makeMdForPlugins.js
@@ -13,9 +13,7 @@ $content
 
 
 
-    let tmp = `<tr><td>
-  <img src="$icon" width="32px" height="32px">
-</td><td><a href="$url"><em>$name</em></a></td><td>$desc</td></tr>`
+    let tmp = `<tr><td><img src="$icon" width="32" height="32"></td><td><a href="$url"><em>$name</em></a></td><td>$desc</td></tr>`
     let content = ''
     plugins.forEach(plugin => {
         let pluginContent = fs.readFileSync(path.join(Plugin_path, plugin), 'utf8')
