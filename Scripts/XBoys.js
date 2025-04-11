84d4c40a47c6eeffbc2a1541d6fe1cb8584f7f2d
zyzdai <73833115+zyzdai@users.noreply.github.com>
2025-04-11 23:56:52 +0800

    更新 XBoys.js

--- Scripts/XBoys.js
+++ Scripts/XBoys.js
@@ -1,6 +1,14 @@
 let res = JSON.parse($response.body);
+let m = url.match(/Account\/IndexDetail/)
+if(m){
 res.vipEndTime = "2999-09-09会员到期";
 res.cartoonVip = true;
 res.vipLevel = 2;
 res.nickName = "Wuang ོ";
 $done({body:JSON.stringify(res)});
+}
+m = url.match(/api\/movie\/DetailInfo/)
+if(m){
+res.movie.isFree = true;
+$done({body:JSON.stringify(res)});
+}
