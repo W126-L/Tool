d42282f9d92a45fa8a954866e8f84860533de221
W126-L <126219250+W126-L@users.noreply.github.com>
2025-04-20 23:10:54 +0800

    创建 KaoShiBao.js

---
+++ Scripts/KaoShiBao.js
@@ -0,0 +1,16 @@
+/**************************
+\考\试\宝\ \部\分\功\能\解\锁\
+\广\告\拦\截\
+
+
+
+
+[mitm]
+hostname = api.ankianki.com
+
+[rewrite_local]
+https:\/\/api\.ankianki\.com url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/ksb.js
+https:\/\/api\.ankianki\.com\/banner\/get url reject-200
+https:\/\/api\.ankianki\.com\/popupAd\/get url reject-200
+https:\/\/api\.ankianki\.com\/ad url reject-200
+ ***************/
