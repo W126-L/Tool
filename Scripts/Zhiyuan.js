aba37f5b6fb580157c1eaebc81995caf9a68b4da
zyzdai <73833115+zyzdai@users.noreply.github.com>
2025-10-29 17:21:18 +0800

    更新 Zhiyuan.js

--- Scripts/Zhiyuan.js
+++ Scripts/Zhiyuan.js
@@ -2,12 +2,5 @@
 app：知源经络穴位
 */
 let obj = JSON.parse($response.body);
-const date = new Date();
-date.setDate(date.getDate() + 33333);
-const year = date.getFullYear();
-const month = String(date.getMonth() + 1).padStart(2, '0');
-const day = String(date.getDate()).padStart(2, '0');
-
-const formatted = `${year}-${month}-${day}`;
-obj.data.vipEndedAt=formatted;
+obj.data.vipEndedAt="2100-01-02";
 $done({ body: JSON.stringify(obj) });
