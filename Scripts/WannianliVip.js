2e98ce3de0fb216cf1a154cd04e530f9ca7d3aa3
W126-L <126219250+W126-L@users.noreply.github.com>
2025-11-10 11:31:58 +0800

    更新 WannianliVip.js

--- Scripts/WannianliVip.js
+++ Scripts/WannianliVip.js
@@ -3,13 +3,13 @@ let m = url.match(/api\/Member\/IsMember/)
 if(m){
 let obj = JSON.parse($response.body);
 obj.data.isMember = true;
-obj.data.endTime = "2166-01-01 00:00:00";
+obj.data.endTime = "2099-12-31 23:59:59";
 $done({ body: JSON.stringify(obj) });
 }
 m = url.match(/Api\/User\/GetExtInfo/)
 if(m){
 let obj = JSON.parse($response.body);
 obj.data.memberType = 1;
-obj.data.memberDate = "2166-01-01 00:00:00";
+obj.data.memberDate = "2099-12-31 23:59:59";
 $done({ body: JSON.stringify(obj) });
 }
