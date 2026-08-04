71c514e032ead82ee11d409d0b81e51e8ad1de08
zyzdai <73833115+zyzdai@users.noreply.github.com>
2026-08-04 23:54:06 +0800

    更新 Earthquake.js

--- Scripts/Earthquake.js
+++ Scripts/Earthquake.js
@@ -3,12 +3,25 @@ app：地址预警
 [rewrite_local]
 https://mobile-new.chinaeew.cn/v1/order/apple/vip url script-response-body https://raw.githubusercontent.com/W126-L/Tool/master/Scripts/Earthquake.js
 hostname = mobile-new.chinaeew.cn
-*/
+
 let obj = JSON.parse($response.body);
 const timestamp = Date.now() + 24 * 60 * 60 * 1000;
 obj.data = {
   "productName" : "SVIP",
   "endTime" : timestamp,
+  "vipType" : "svip",
+  "productId" : "earthquake_early_warning_SVIP",
+  "upgradePrice" : 0
+};
+$done({ body: JSON.stringify(obj) });
+
+
+
+*/
+let obj = JSON.parse($response.body);
+obj.data = {
+  "productName" : "SVIP",
+  "endTime" : 4791705729000,
   "vipType" : "svip",
   "productId" : "earthquake_early_warning_SVIP",
   "upgradePrice" : 0
